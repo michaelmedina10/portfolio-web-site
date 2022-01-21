96ac4f83bbff99810edb5d35aee7ac3b723a4873
Michael Medina <michael.cunha97@hotmail.com>
2022-01-21 18:20:58 -0300

    Adding pages of hobby and say hello as ajax concept

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+console.log(window.location);
+
 document.querySelectorAll(".accessSubMenu").forEach((accSubMenu) => {
   console.log(accSubMenu.nextElementSibling);
   accSubMenu.onclick = (event) => {
@@ -5,12 +7,25 @@ document.querySelectorAll(".accessSubMenu").forEach((accSubMenu) => {
   };
 });
 
+function toggle(element, classe) {
+  element.classList.toggle(classe);
+}
+
 document.querySelectorAll(".btnMenu").forEach((btn) => {
   btn.onclick = (event) => {
     const btn = event.target;
     const leftMenu = document.querySelector(".left");
-    console.log(leftMenu);
-    leftMenu.classList.toggle("menuActive");
-    console.log(leftMenu);
+    toggle(leftMenu, "menuActive");
+  };
+});
+
+document.querySelectorAll("[ajaxAttrib]").forEach((link) => {
+  console.log(link);
+  link.onclick = (event) => {
+    event.preventDefault();
+    const mainContainer = document.querySelector(".mainContainer");
+    fetch(link.getAttribute("href"))
+      .then((response) => response.text())
+      .then((html) => (mainContainer.innerHTML = html));
   };
 });
