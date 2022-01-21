2561ae37f14a9c079825070d10ccd833488fbbd3
Michael Medina <michael.cunha97@hotmail.com>
2022-01-21 12:52:52 -0300

    Adding the function of the buttons when the user being looking on the phone

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ document.querySelectorAll(".accessSubMenu").forEach((accSubMenu) => {
     accSubMenu.nextElementSibling.classList.toggle("mostrar");
   };
 });
+
+document.querySelectorAll(".btnMenu").forEach((btn) => {
+  btn.onclick = (event) => {
+    const btn = event.target;
+    const leftMenu = document.querySelector(".left");
+    console.log(leftMenu);
+    leftMenu.classList.toggle("menuActive");
+    console.log(leftMenu);
+  };
+});
